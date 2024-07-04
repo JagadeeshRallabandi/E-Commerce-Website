a010343bcee6a06cd3877e5667951868773a2384
JagadeeshRallabandi <rjagadeeshnit@gmail.com>
2024-07-04 16:10:47 +0530

    Last Commit

---
+++ ecommbot/data_converter.py
@@ -0,0 +1,24 @@
+import pandas as Pd
+from langchain_core.documents import Document
+
+def dataconveter():
+    product_data=pd.read_csv("D:\\Gen_AI_Sunny_Savith\\E-Commerce-Website\\data\\flipkart_product_review.csv")
+
+    data=product_data[["product_title","review"]]
+
+    product_list = []
+
+    for index, row in data.iterrows():
+        obj = {
+                'product_name': row['product_title'],
+                'review': row['review']
+            }
+        product_list.append(obj)
+
+               
+    docs = []
+    for entry in product_list:
+        metadata = {"product_name": entry['product_name']}
+        doc = Document(page_content=entry['review'], metadata=metadata)
+        docs.append(doc)
+    return docs
